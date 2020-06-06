b9d55548d19a0134cee7566a49bac42b37aa399f
hightechgrace <hans@hightechgrace.org>
2020-06-06 02:17:00 -0500

    Add project files.

---
+++ WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace AMS_UMC_LIVE
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
